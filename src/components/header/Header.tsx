9c5829db3f02f4a2439162e2e813dc8f756b925d
Arrasyid Fadel Fatonsyah <aff.anton20@gmail.com>
2024-06-18 11:06:51 +0700

    feat: handle path alias

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.scss'
+import person from '../../assets/person.png'
+
+function Header() {
+  return (
+    <div>
+        {/* <img src={person} alt="logo" /> */}
+    </div>
+  )
+}
+
+export default Header
