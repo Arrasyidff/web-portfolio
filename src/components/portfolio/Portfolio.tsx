9accdbb6459bcec033a958033f7b7b614fba3dda
Arrasyid Fadel Fatonsyah <aff.anton20@gmail.com>
2024-06-23 19:49:58 +0700

    refactor: update handle portfolio view

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,50 +1,44 @@
 import './style.scss'
 
 function Portfolio() {
-  return (
-    <div className='wp-portfolio__container section--padding'>
-        <h1 className='section--title'>PORTFOLIO.</h1>
-
-        <div className='wp-portfolio__items'>
-            <div className='wp-portfolio__item'>
-                <h2 className='sub--title'>E-Commerce.</h2>
-                <div className='wp-portfolio__item-img'>
-
-                </div>
-            </div>
-            <div className='wp-portfolio__item'>
-                <h2 className='sub--title'>E-Commerce.</h2>
-                <div className='wp-portfolio__item-img'>
-
-                </div>
-            </div>
-            <div className='wp-portfolio__item'>
-                <h2 className='sub--title'>E-Commerce.</h2>
-                <div className='wp-portfolio__item-img'>
-
-                </div>
-            </div>
-            <div className='wp-portfolio__item'>
-                <h2 className='sub--title'>E-Commerce.</h2>
-                <div className='wp-portfolio__item-img'>
-
-                </div>
-            </div>
-            <div className='wp-portfolio__item'>
-                <h2 className='sub--title'>E-Commerce.</h2>
-                <div className='wp-portfolio__item-img'>
-
-                </div>
-            </div>
-            <div className='wp-portfolio__item'>
-                <h2 className='sub--title'>E-Commerce.</h2>
-                <div className='wp-portfolio__item-img'>
-
-                </div>
-            </div>
+    return (
+        <div className='wp-portfolio__container section--padding'>
+            <h1 className='section--title'>PORTFOLIO.</h1>
+                {((data): any => {
+                    if (data.length == 0) return (
+                        <p className='sub--title'>Uploading soon!</p>
+                    )
+                    return (
+                        <>
+                            <div className='wp-portfolio__items'>
+                                {
+                                    data.map((item: Portfolio, i: number) => (
+                                        <div key={i} className='wp-portfolio__item'>
+                                            <h2 className='sub--title'>{item.name}</h2>
+                                            <div className='wp-portfolio__item-img'>
+                            
+                                            </div>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        </>
+                    )
+                })(PORTFOLIOS)}
         </div>
-    </div>
-  )
+    )
 }
+
+type Portfolio = {
+    name: string
+    link: string
+} 
+const PORTFOLIOS: Portfolio[] = [
+    // {name: 'Ecommerce', link: ''},
+    // {name: 'Ecommerce', link: ''},
+    // {name: 'Ecommerce', link: ''},
+    // {name: 'Ecommerce', link: ''},
+    // {name: 'Ecommerce', link: ''}
+]
 
 export default Portfolio
